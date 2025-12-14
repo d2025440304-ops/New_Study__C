2965c3f079ac64b37ab02e74373ce3aef3862f54
Yu-dai <d2025440304@gmail.com>
2025-12-14 17:11:06 +0800

    Re_FILE_operation_

---
+++ Review/18.Re_FILE_op.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+// int main()
+// {
+//     //fopen有一个返回值，每次打开文件就会有一个文件信息，用一个 FILE 指针来指向这个文件信息
+//     //流的概念
+//     //流是一个在用户的与键盘，网络，硬盘，屏幕之间来更好的帮助程序员来处理这些
+    
+//     //打开文件
+//     FILE * pf = fopen("test.txt","w");
+//     if(pf == NULL)
+//     {
+//         perror("fopen");
+//         return 1;
+//     }
+//     //操作文件
+//     while((fgetc(pf)) != EOF)
+//     {
+//             ;
+//     }
+
+
+//     fclose(pf);
+//     pf = NULL;
+//     return 0;
+// }
+int main()
+{
+    printf("%d",__STDC__);
+    return 0;
+}
