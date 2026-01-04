2522bd96797cd051b190a6ba451eca29825630a4
Yu-dai <d2025440304@gmail.com>
2026-01-04 21:11:46 +0800

    链表题目

---  Repare_Competition/12_22.c
+++  Repare_Competition/12_22.c
@@ -1,23 +1,65 @@
-int removeElement(int* nums, int numsSize, int val)
+// int removeElement(int* nums, int numsSize, int val)
+// {
+//     //定义双指针，一个快指针搜索，一个慢指针被覆盖
+//     //在双指针的解题里大多都用的是反面的思想
+//     //当 src 指向value 时，src++，会将 value 留下来给dest，当 src 遇到不是 value 时，将 src 的值赋给 dest，也就是将 value 覆盖，这样 dest 加几次，就证明覆盖了几次 value，也就是数组中的真实有效元素
+//     int src,dest;
+//     while(src < numsSize)
+//     {
+//         if(nums[src] == val)
+//         {
+//             src++;
+//         }
+//         else 
+//         {
+//             nums[dest] = nums[src];
+//             src++;
+//             dest++;
+//         }
+//     }
+//     return dest;
+// }
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+ typedef struct ListNode ListNode;
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) 
 {
-    //定义双指针，一个快指针搜索，一个慢指针被覆盖
-    //在双指针的解题里大多都用的是反面的思想
-    //当 src 指向value 时，src++，会将 value 留下来给dest，当 src 遇到不是 value 时，将 src 的值赋给 dest，也就是将 value 覆盖，这样 dest 加几次，就证明覆盖了几次 value，也就是数组中的真实有效元素
-    int src,dest;
-    while(src < numsSize)
+    //考虑空链表的情况
+    if(list1==NULL)//如果 p1 等于空
+    return list2;
+    if(list2==NULL)
+    return list1;
+    //双指针
+    ListNode *p1 = list1;
+    ListNode *p2 = list2;
+    //哨兵位
+    ListNode * phead,*ptail;
+    ptail = phead = (ListNode *)malloc(sizeof(ListNode));
+    while(p1 && p2)
     {
-        if(nums[src] == val)
+        if(p1->val > p2->val)
         {
-            src++;
-        }
-        else 
-        {
-            nums[dest] = nums[src];
-            src++;
-            dest++;
+            ptail->next = p2;
+            ptail = ptail->next;
+            p2 = p2->next;
+        }else{
+            ptail->next = p1;
+            ptail = ptail->next;
+            p1 = p1->next;
         }
     }
-    return dest;
+    if(p1)//如果p1 不为空
+    ptail->next = p1;
+    if(p2)
+    ptail->next = p2;
+    ListNode * ret = phead->next;
+    free(phead);
+    phead = NULL;
+    return ret;
 }
-
-
---
+++ Review/20.SLList.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+typedef int SLDataType;
+typedef struct SLList{
+    SLDataType data;
+    struct SLList *next;
+}SLTNode;
+//打印
+void SLList_print(SLTNode * phead,SLDataType x)
+{
+    assert(phead);
+    while(phead)
+    {
+        printf("%d->",phead->data);
+        phead = phead->next;
+    }
+    printf("NLLL\n");
+}
+
+SLTNode *  SLT_Buy_Node(SLDataType x)
+{
+    SLTNode * pnewnode = (SLTNode *)maclloc(sizeof(SLTNode));
+    if(pnewnode == NULL)
+    {
+        perror("malloc fail !");
+        return ;
+    }
+    return pnewnode;
+}
+
+//尾插
+void SLList_Push_Back(SLTNode ** pphead,SLDataType x)
+{
+    assert(pphead);
+    SLTNode * pnewnode = SLT_Buy_Node(x);
+    if(*pphead == NULL)
+    {
+        *pphead = pnewnode;
+    }else{  
+        SLTNode *pcur = *pphead;
+        while(pcur->next)
+        {
+            pcur = pcur->next;
+        }
+        pcur->next = pnewnode;
+    }
+}
+
+//尾删
+void SLTPop_Back(SLTNode **pphead)
+{
+    assert(pphead && *pphead);
+
+} 
